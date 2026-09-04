d8091aeb720a53ebb9c0c52084372a2c4a3c307c
Lim <779793115@qq.com>
2026-09-04 15:30:51 +0800

    feat: register free viral collectors

--- packages/discovery/src/collectors/index.ts
+++ packages/discovery/src/collectors/index.ts
@@ -1,7 +1,10 @@
 import type { Collector, SignalSourceType } from "@factory/shared"
+import { hnCollector } from "./hn.js"
 import { officialApiCollector } from "./official-api.js"
 import { redditCollector } from "./reddit.js"
+import { rssCollector } from "./rss.js"
 import { sitemapCollector } from "./sitemap.js"
+import { twitchCollector } from "./twitch.js"
 import { wikiCollector } from "./wiki.js"
 import { wikiGgCollector } from "./wiki-gg.js"
 import { xCollector } from "./x.js"
@@ -13,6 +16,9 @@ const collectors: Partial<Record<SignalSourceType, Collector>> = {
   wiki_gg: wikiGgCollector,
   reddit: redditCollector,
   youtube: youtubeCollector,
+  twitch: twitchCollector,
+  hn: hnCollector,
+  rss: rssCollector,
   x: xCollector,
   sitemap: sitemapCollector
 }
@@ -21,5 +27,8 @@ export function getCollector(type: SignalSourceType): Collector | undefined {
   return collectors[type]
 }
 
+export * from "./hn.js"
+export * from "./rss.js"
 export * from "./sitemap.js"
+export * from "./twitch.js"
 export * from "./wiki-gg.js"
