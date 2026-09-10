7c5f657de07eee9667381a3ef32c1c536a6f0e35
Lim <779793115@qq.com>
2026-09-10 20:14:19 +0800

    feat: enable Viral Radar stat card filtering

--- apps/dashboard/src/main.tsx
+++ apps/dashboard/src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import App from "./App"
+import { installViralRadarCardFilters } from "./viral-radar-filter"
 import "./styles.css"
 import "./keywords.css"
+import "./viral-radar.css"
 
 createRoot(document.getElementById("root")!).render(<StrictMode><App /></StrictMode>)
+installViralRadarCardFilters()
